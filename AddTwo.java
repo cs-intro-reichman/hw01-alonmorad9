8c1f1e99d142162754f2a0de7899968de34c221f
alonmorad9 <alon.morad@post.runi.ac.il>
2023-12-20 18:05:06 +0200

    Homework Update

--- AddTwo.java
+++ AddTwo.java
@@ -6,9 +6,7 @@ public class AddTwo {
 		// Declares two integer variables and gets them from user
         int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
-		// prints the program name and user input
-        System.out.println("% java AddTwo " + a + " " + b);
 		// prints the sum=output 
-		System.out.println(a + "+" + b + "=" + (a+b));	
+		System.out.println(a + " + " + b + " = " + (a+b));	
 	}
 }
