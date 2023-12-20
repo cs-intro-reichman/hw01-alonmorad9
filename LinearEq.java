853cf6dd4126ec156ee63afc070e398625ae64db
alonmorad9 <alon.morad@post.runi.ac.il>
2023-12-20 18:18:23 +0200

    homework update

--- LinearEq.java
+++ LinearEq.java
@@ -5,13 +5,13 @@
  * Treats the three arguments as well as the computed value as double values
  */
 public class LinearEq {
+	public static void main(String[] args) {
 		// declares three double variables and gets them from user
         double a = Double.parseDouble(args[0]);
 		double b = Double.parseDouble(args[1]);
 		double c = Double.parseDouble(args[2]);
-		// prints the program name and user input
-        System.out.println("% java LinearEq " + a + " " + b + " " + c);
 		// prints the equation and its solution
 		System.out.println(a + " *" + " X" + " + " + b + " = " + c);
 		System.out.println(" X " + "= " + ((c-b)/a));
+	}
 }
